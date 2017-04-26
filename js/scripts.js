8eadc2d2d3224d06a1e04be18971a736d4fc3b91
Daniel Lopez and Michael Koceja <michael@>
2017-04-26 15:48:34 -0700

    add food survey question, add paragraph with spans to #thanks

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 // back-end
 
-var flavor = $("input:radio[name=flavor]:checked").val();
 
 // front-end
 
 $(function() {
   $("#form-btn").submit(function(event) {
+
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    var foods = $("input:radio[name=foods]:checked").val();
+
+    $(".flavor").text(flavor);
+    $(".foods").text(foods);
+
     $("#thanks").show();
 
     event.preventDefault();
